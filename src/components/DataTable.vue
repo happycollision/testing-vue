cf4aa4d676b73d7ce1fcf433491c5c774e650d93
Don Denton <don@happycollision.com>
2018-12-10 23:40:41 -0600

    Transform csv string to data and display as table

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="hello">
-    <span>{{ data }}</span>
+    <table>
+      <tr>
+        <th v-for="label in tableData.header" :key="label">{{label}}</th>
+      </tr>
+      <tr v-for="row in tableData.rows" :key="row.id">
+        <td v-for="item in row" :key="item">{{item}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -8,8 +15,27 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class HelloWorld extends Vue {
+export default class DataTable extends Vue {
   @Prop() private data!: string;
+  get tableData() {
+    const [header, ...rows] = this.data.split('\n')
+      .map((line) => line.split(',')
+        .reduce((a, c, i) => {
+          if (i === 0) { return [c]; }
+          const aIndex = a.length - 1;
+          const last = a[aIndex];
+          const hadOpener = /^\s*"/.test(last);
+          const hasCloser = /"\s*$/.test(c);
+          if (hadOpener) { a[aIndex] = `${last},${c}`; }
+          if (hadOpener && hasCloser) { a[aIndex] = a[aIndex].trim().substring(1, a[aIndex].length - 1); }
+          if (!hadOpener && hasCloser) { throw new Error('closing quote found without opening quote'); }
+          if (!hadOpener && !hasCloser) { a.push(c); }
+          return a;
+        }, [] as string[])
+        .map((cell) => cell.trim()),
+      );
+    return {header, rows};
+  }
 }
 </script>
 
--- tests/unit/DataTable.spec.ts
+++ tests/unit/DataTable.spec.ts
@@ -4,7 +4,7 @@ import DataTable from '@/components/DataTable.vue';
 
 describe('HelloWorld.vue', () => {
   it('shows the headers and data', () => {
-    const data = 'Header 1;Header 2\nData 1; Data 2';
+    const data = 'Header 1,Header 2\nData 1, Data 2';
     const wrapper = shallowMount(DataTable, {
       propsData: { data },
     });
@@ -12,5 +12,28 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).to.include('Header 2');
     expect(wrapper.text()).to.include('Data 1');
     expect(wrapper.text()).to.include('Data 2');
+  });
+
+  it('data is in a proper table', () => {
+    const data = 'Header 1,Header 2\nData 1, Data 2';
+    const wrapper = shallowMount(DataTable, {
+      propsData: { data },
+    });
+    const headerCols = wrapper.findAll('th');
+    const cells = wrapper.findAll('td');
+    expect(headerCols.at(0).text()).to.include('Header 1');
+    expect(headerCols.at(1).text()).to.include('Header 2');
+    expect(cells.at(0).text()).to.include('Data 1');
+    expect(cells.at(1).text()).to.include('Data 2');
+  });
+
+  it('data is properly escaped', () => {
+    const data = 'Header 1,Header 2\n"Data 1 with, quotes", Data 2';
+    const wrapper = shallowMount(DataTable, {
+      propsData: { data },
+    });
+    const headerCols = wrapper.findAll('th');
+    const cells = wrapper.findAll('td');
+    expect(cells.at(0).text()).to.include('Data 1 with, quotes');
   });
 });
