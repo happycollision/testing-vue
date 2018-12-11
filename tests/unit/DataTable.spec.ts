ac93bec5960d7cecdd93cfdeebf4ed3f0008f172
Don Denton <don@happycollision.com>
2018-12-11 00:21:04 -0600

    Failing test for sorting after a button click

--- tests/unit/DataTable.spec.ts
+++ tests/unit/DataTable.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import DataTable from '@/components/DataTable.vue';
 
-describe('HelloWorld.vue', () => {
+describe('DataTable.vue', () => {
   it('shows the headers and data', () => {
     const data = 'Header 1,Header 2\nData 1, Data 2';
     const wrapper = shallowMount(DataTable, {
@@ -32,8 +32,26 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(DataTable, {
       propsData: { data },
     });
-    const headerCols = wrapper.findAll('th');
     const cells = wrapper.findAll('td');
     expect(cells.at(0).text()).to.include('Data 1 with, quotes');
+  });
+
+  it('data can be sorted', (done) => {
+    const data = 'Header 1,Header 2\n3,3\n20,20\nb,b\na,a\n1,1';
+    const wrapper = shallowMount(DataTable, {
+      propsData: { data },
+    });
+
+    wrapper.find('button').trigger('click');
+
+    wrapper.vm.$nextTick(() => {
+      const rows = wrapper.findAll('tr');
+      expect(rows.at(1).text()).to.include('11');
+      expect(rows.at(2).text()).to.include('33');
+      expect(rows.at(3).text()).to.include('2020');
+      expect(rows.at(4).text()).to.include('bb');
+      expect(rows.at(5).text()).to.include('aa');
+      done();
+    });
   });
 });
