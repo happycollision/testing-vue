606ff4ad79e47858cd0b85e4eef824d74da13fbe
Don Denton <don@happycollision.com>
2018-12-13 17:19:12 -0600

    Add description.js [ci skip]

--- npm-scripts/finish-build.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-
-
-const pathToIndex = path.join(__dirname, '..', 'dist', 'index.html')
-const file = fs.readFileSync(pathToIndex, {encoding: 'utf-8'});
-
-fs.writeFileSync(pathToIndex, file.replace(/="(\/.*?)"/g, '="https://happycollision.github.io/testing-vue$1"'))
--- src/types/table-types.ts
+++
@@ -1,9 +0,0 @@
-export interface Row {
-  [s: string]: string | number | Date;
-}
-
-export interface EditEvent {
-  row: Row;
-  column: string;
-  value: string | number;
-}
