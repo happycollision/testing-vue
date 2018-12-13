ea9fc3eeddb625d698147b611eea323b16406654
Don Denton <don@happycollision.com>
2018-12-13 14:28:04 -0600

    Add instructions for visitors.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div id="app">
+  <div id="app" class="p-1">
+    <div class="max-w-md text-left m-auto p-2">
+      <h1 class="text-center">Testing Vue</h1>
+      <p>This is my first attempt at using Vue at all. I like the tools that Vue's CLI provides though they actually seemed a little buggy at times (often a simple stop and restart did the trick).</p>
+      <details>
+        <summary class="cursor-pointer">Instructions</summary>
+        <p>You can tab through the interactable fields below, or click.</p>
+        <p>The column header buttons cycle through sort ascending, descending, then none with each click.</p>
+        <p>The search field acts more like a filter. Any text inside a row is searched. If no matches are found, the row is removed from the view. Click "clear" to wipe out your search text.</p>
+        <p>Clicking directly on a desciption allows you to edit that description. Pressing <code class="bg-grey-light p-1">enter</code> or leaving the field will commit the changes.</p>
+        <p>Clicking on a row (not the description, but anywhere else) selects that row. With multiple rows selected, any edit you make to a description is applied to all selected (and visible) rows' descriptions.</p>
+        <p>Clicking on the lone checkbox at the top of the left-most column (the ID column) selects all visible rows. Clicking again deselects all of them.</p>
+      </details>
+    </div>
     <DataTable
       :header="allTableData.header"
       :rows="allTableData.rows"
@@ -42,6 +55,10 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+p {
+  margin-top: 1em;
+  margin-bottom: 1em;
 }
 </style>
